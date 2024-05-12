d237d81daea78ef8a2716c2a6c06ef73dd415b2d
mackeyaddict <naufalyusuf90@gmail.com>
2024-05-12 07:55:10 +0700

    style: :art: add responsive to chatbot section in home page

--- src/pages/home/home-chatbot.jsx
+++ src/pages/home/home-chatbot.jsx
@@ -6,12 +6,12 @@ import { PAGE_URL } from "../../utils/constant";
 export default function HomeChatbot() {
   return (
     <section className="pb-12 px-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="w-[386px]">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+        <div className="w-[386px] hidden md:block">
           <img src={chatBotImg} alt="Chatbot Img" />
         </div>
-        <div className="flex flex-col gap-3 items-end"> 
-          <h1 className="text-5xl font-semibold text-end">
+        <div className="flex flex-col gap-3 items-center md:items-end"> 
+          <h1 className="text-5xl font-semibold text-center md:text-end">
             Wanna know anything <br /> about coffee?
           </h1>
           <div className="max-w-xs">
