e4e3c294f27dd3834bf1e793f936cfd4b4146706
mackeyaddict <naufalyusuf90@gmail.com>
2024-05-13 13:15:26 +0700

    style: :lipstick: fix detail img product responsive issues

--- src/pages/detail-product/detail-img-product.jsx
+++ src/pages/detail-product/detail-img-product.jsx
@@ -1,6 +1,6 @@
 export default function DetailImgProdut( {product} ) {
   return (
-    <div className="bg-[#F4F4F4] w-[381px] h-[381px] md:w-[481px] md:h-[481px] flex items-center rounded-[20px]">
+    <div className="bg-[#F4F4F4] w-[350px] h-[350px] md:w-[481px] md:h-[481px] flex items-center rounded-[20px]">
       <img src={product.productImgUrl} alt={product.productName}  className="transition-transform transform hover:scale-110"/>
     </div>
   )
