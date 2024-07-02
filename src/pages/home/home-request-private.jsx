629d9d8654a7cb7078f636ecca51fdf82bff78fd
mackeyaddict <naufalyusuf90@gmail.com>
2024-07-02 20:09:55 +0700

    feat: add modal to display comming soon feature

--- src/pages/home/home-request-private.jsx
+++ src/pages/home/home-request-private.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
 import requestPrivateBg from "../../assets/images/request-private-bg.jpg";
 import Button from "../../components/button";
+import Modal from "../../components/modal";
 
 export default function HomeRequestPrivate() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <section className="pb-12 px-4">
       <div
@@ -17,12 +29,17 @@ export default function HomeRequestPrivate() {
             celebrations, or other events
           </p>
           <div className="max-w-[146px]">
-            <Button variant="primary" size="md">
+            <Button variant="primary" size="md" onClick={openModal}>
               Booking Now
             </Button>
           </div>
         </div>
       </div>
+      <Modal open={isModalOpen} onClose={closeModal}>
+        <div className="flex items-center justify-center p-32 h-full">
+          <h2 className="text-6xl font-semibold">SOON!</h2>
+        </div>
+      </Modal>
     </section>
   );
 }
