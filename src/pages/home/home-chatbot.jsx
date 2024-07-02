1f775d57230be3a485bf55299728654e310658e5
mackeyaddict <naufalyusuf90@gmail.com>
2024-07-02 20:08:37 +0700

    style: :lipstick: adding coffee chatbot icon

--- src/pages/home/home-chatbot.jsx
+++ src/pages/home/home-chatbot.jsx
@@ -7,16 +7,16 @@ export default function HomeChatbot() {
   return (
     <section className="pb-12 px-4">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-        <div className="w-[386px] hidden md:block">
+        <div className="w-[286px] hidden md:block animate-bounce">
           <img src={chatBotImg} alt="Chatbot Img" />
         </div>
         <div className="flex flex-col gap-3 items-center md:items-end"> 
-          <h1 className="text-5xl font-semibold text-center md:text-end">
+          <h1 className="text-4xl font-semibold text-center md:text-end">
             Wanna know anything <br /> about coffee?
           </h1>
           <div className="max-w-xs">
             <Link to={PAGE_URL.CHATBOT}>            
-              <Button variant="secondary" size="xl">
+              <Button variant="secondary" size="lg">
                 Ask Roastmaster
               </Button>
             </Link>
